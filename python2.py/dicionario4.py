ff160d85d1f6a1c29c9f15ca0db727a37e6a4a6e
Gabriel da Costa Peres <105747318+gabrielperes16@users.noreply.github.com>
2024-06-26 12:16:26 -0300

    Add files via upload

--- python2.py/dicionario4.py
+++ python2.py/dicionario4.py
@@ -1,18 +1,42 @@
-soma=0
-registro={}
-registro['nome']=str(input("Digite o nome do jogador: "))
-registro['partidas']=int(input("Quantas partidas este jogador jogou?"))
-for partida in range(1,registro['partidas']+1):
-    registro['gols']=int(input("Digite a quantidade de gols: "))
-    soma+=registro['gols']
-registro['total']=soma
+time=list()
+jogador = {}
+partidas= []
+while True:
+    jogador.clear()
+    jogador['nome'] = str(input("Digite o nome do jogador: "))
+    tot=int(input("Quantas partidas este jogador jogou?"))
+    for c in range(0, tot):
+        partidas.append(int(input("Digite a quantidade de gols: ")))
+    jogador['gols']=partidas[:]
+    jogador['total']=sum(partidas)
+    time.append(jogador.copy())
+    while True:
+        continuar=str(input("Quer continuar?  [ S ] ou [ N ]")).upper()[0]
+        if continuar in "SN":
+            break
+        print("Responda corretamente!")
+    if continuar=='N':
+        break
 print("-="*30)
-print(registro)
-print("-="*30)
-for k,v in registro.items():
-    print(f"O campo {k} tem o valor {v}")
-print("-="*30)
-print(f"O jogador {registro["nome"]} jogou {registro['partidas']}")
-for i, v in enumerate(registro['gols']):
-    print(f"     => na partida {i}, fez {v} gols")
-print(f"Foi um total de {registro['total']}gols")
+print("Cod", end='')
+for i in jogador.keys():
+    print(f"{i:<15}", end='')
+print()
+print("-"*40)
+for k,v in enumerate(time): 
+    print(f'{k}     ',end='')
+    for d in v.values():
+        print(f"{str(d):<15}", end='')
+    print()
+print("-"*40)
+while True:
+    busca=int(input("MOstra os dados de qual jogador:"))
+    if busca == 999:
+        break
+    if busca >= len(time):
+        print("Jogador não encontrado!")
+    else:
+        print(f"-- LEVANTAMENTO DO JOGADOR {time[busca]['nome']}:")
+        for i,g in enumerate(time[busca]['gols']):
+            print(f"        no jogo {i} fez {g} gols")
+        print('=='*30)
